13c8c42a554f88fbcca9b0cc07bccfbbe3cb210b
Chanwoong Kim <me@chanwoong.kim>
2019-07-13 16:29:09 +0900

    Adjust column sizes

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -27,14 +27,14 @@ export default {
         { text: 'Hunt', value: 'hunt', class: 'pa-1', sortable: true, width: 70 },
         { text: 'Abyss', value: 'abyss', class: 'pa-1', sortable: true, width: 70 },
         { text: 'Raid', value: 'raid', class: 'pa-1', sortable: true, width: 70 },
-        { text: 'PvE AVR', value: 'pve_average', class: 'pa-1', sortable: true, width: 50 },
-        { text: 'Recommended Set', value: 'recommended_set', class: 'pa-1' },
+        { text: 'PvE AVR', value: 'pve_average', class: 'pa-1', sortable: true, width: 80 },
+        { text: 'Recommended Set', value: 'recommended_set', class: 'pa-1', width: 150 },
         { text: 'Neck', value: 'neck', class: 'pa-1' },
         { text: 'Suggested Roles', value: 'suggested_roles', class: 'pa-1', width: 150 },
         { text: 'Recommended Artifact', value: 'recommended_artifact', class: 'pa-1', width: 160, type: 'img' },
         { text: 'Alternative Artifact', value: 'alternate_artifact', class: 'pa-1', width: 160, type: 'img' },
         { text: 'Alternative Artifact', value: 'alternate_artifact2', class: 'pa-1', width: 160, type: 'img' },
-        { text: 'Note', value: 'note', class: 'pa-1 cl-tier-note', width: 400 }
+        { text: 'Note', value: 'note', class: 'pa-1 cl-tier-note', width: 420 }
       ],
 
       tiers: [
