16824b23cc2df8aefe2420168426803385061587
Chanwoong Kim <me@chanwoong.kim>
2019-07-22 23:55:47 +0900

    Add credits page

---
+++ web/pages/credits.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-card class="box-card">
+    <div>
+      <p>
+        A toy project for learning Vue.js
+      </p>
+      <p>
+        Display the Epic Seven Game character tier list.
+      </p>
+      <p>
+        Data source: <a href="https://www.reddit.com/r/EpicSeven/comments/aqb7jn/new_tier_list_version_is_here/">/r/EpicSeven Subreddit</a>.
+      </p>
+    </div>
+  </el-card>
+</template>
