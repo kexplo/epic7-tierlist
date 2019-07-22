689c101eb53460f569ec9d0f9803ab9c4440a7c1
Chanwoong Kim <me@chanwoong.kim>
2019-07-22 23:46:23 +0900

    Add PvP TierList

---
+++ web/pages/pvp.vue
@@ -0,0 +1,85 @@
+<template>
+  <tier-table v-model="search" :headers="headers" :items="tiers" title="Epic 7 Tier List (PvP)" />
+</template>
+
+<script>
+import TierTable from '~/components/TierTable.vue'
+import pvp_data from '~/static/pvp.json'
+
+/*
+TODO: USE VALUE. NOT URL
+const classFilters = [
+  { text: 'Warrior', value: 'Warrior' },
+  { text: 'Knight', value: 'Knight' },
+  { text: 'Ranger', value: 'Ranger' },
+  { text: 'Soul Weaver', value: 'Soul Weaver' },
+  { text: 'Mage', value: 'Mage' },
+  { text: 'Thief', value: 'Thief' },
+  ];
+*/
+const classFilters = [
+  { text: 'Warrior', value: 'https://i.imgur.com/a34GyyB.png' },
+  { text: 'Knight', value: 'https://i.imgur.com/3OCQgMB.png' },
+  { text: 'Ranger', value: 'https://i.imgur.com/4UWNMER.png' },
+  { text: 'Soul Weaver', value: 'https://i.imgur.com/TsuJvXE.png' },
+  { text: 'Mage', value: 'https://i.imgur.com/hGAL2QJ.png' },
+  { text: 'Thief', value: 'https://i.imgur.com/YZa7qSv.png' },
+  ];
+
+const elementFilters = [
+  { text: 'Ice', value: 'Ice' },
+  { text: 'Fire', value: 'Fire' },
+  { text: 'Earth', value: 'Earth' },
+  { text: 'Light', value: 'Light' },
+  { text: 'Dark', value: 'Dark' },
+  ];
+const starFilters = [
+  { text: '★★★★★', value: 5 },
+  { text: '★★★★', value: 4 },
+  { text: '★★★', value: 3 },
+  { text: '★★', value: 2 },
+  { text: '★', value: 1 },
+  ];
+
+export default {
+  components: {
+    TierTable
+  },
+
+  data() {
+    return {
+      search: '',
+      pagination: {
+        descending: true,
+        sortBy: 'star'
+      },
+      headers: [
+        { text: '', align: 'left', sortable: false, value: 'img', class: 'pa-1', type: 'img', width: 150, fixed: true },
+        { text: 'Name', value: 'name', class: 'pa-1', sortable: true, width: 150 },
+        { text: '★', value: 'star', class: 'pa-1', sortable: true, width: 70 , filters: starFilters },
+        { text: 'Element', value: 'element', class: 'pa-1', filters: elementFilters },
+        { text: 'Class', value: 'class', class: 'pa-1', type: 'img', filters: classFilters },
+        { text: 'Arena Offense', value: 'arena_offense', class: 'pa-1', sortable: true, width: 70 },
+        { text: 'Arena Defense', value: 'arena_defense', class: 'pa-1', sortable: true, width: 70 },
+        { text: 'GW Offense', value: 'gw_offense', class: 'pa-1', sortable: true, width: 70 },
+        { text: 'GW Defense', value: 'gw_defense', class: 'pa-1', sortable: true, width: 70 },
+        { text: 'PvP AVR', value: 'pvp_average', class: 'pa-1', sortable: true, width: 80 },
+        { text: 'Recommended Set', value: 'recommended_set', class: 'pa-1', width: 150 },
+        { text: 'Neck', value: 'neck', class: 'pa-1' },
+        { text: 'Suggested Roles', value: 'suggested_roles', class: 'pa-1', width: 150 },
+        { text: 'Recommended Artifact', value: 'recommended_artifact', class: 'pa-1', width: 160, type: 'img' },
+        { text: 'Alternative Artifact', value: 'alternate_artifact', class: 'pa-1', width: 160, type: 'img' },
+        { text: 'Alternative Artifact', value: 'alternate_artifact2', class: 'pa-1', width: 160, type: 'img' },
+        { text: 'Alternative Artifact', value: 'alternate_artifact3', class: 'pa-1', width: 160, type: 'img' },
+        { text: 'Note', value: 'note', class: 'pa-1 cl-tier-note', width: 420 }
+      ],
+
+      tiers: [
+      ]
+    }
+  },
+  mounted() {
+    this.tiers = pvp_data;
+  }
+}
+</script>
